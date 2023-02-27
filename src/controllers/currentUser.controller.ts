537f9b5a173ffdf66f0bf7778eba55b5b2cccc9e
josselin <obarayego@gmail.com>
2023-02-27 04:06:11 +0100

    currentUsercontroller

--- src/controllers/currentUser.controller.ts
+++ src/controllers/currentUser.controller.ts
@@ -5,17 +5,20 @@ export const getOrCreate = async (
   res: Response,
   // next: NextFunction,
 ) => {
+  console.log(req.body);
+
   const { authUser } = req.body;
 
   try {
-    let user = await UserModel.findOne({
+    let userDB = await UserModel.findOne({
       sub: authUser?.sub,
     });
-    if (!user) {
-      user = await authUser.save();
+    if (!userDB) {
+      const user = new UserModel({ ...authUser });
+      userDB = await user.save();
     }
 
-    res.status(200).json(user);
+    res.status(200).json(userDB);
   } catch (error) {
     res.status(500).send(error.message);
   }
